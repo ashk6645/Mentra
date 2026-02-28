34df0db3d4caf983a5e05137c6269371050dea44
Ashutosh <ashk6645@gmail.com>
2026-02-28 16:46:25 +0530

    Add responsivness for sidebar for small screen size

--- src/components/layout/page-shell.tsx
+++ src/components/layout/page-shell.tsx
@@ -9,7 +9,7 @@ export function PageShell({ children, className }: PageShellProps) {
     return (
         <div className="flex-1 overflow-y-auto min-h-full no-scrollbar">
             <div className={cn(
-                "max-w-4xl mx-auto px-6 pb-20 pt-16 md:pt-20",
+                "max-w-4xl mx-auto px-4 sm:px-6 pb-20 pt-16 md:pt-20",
                 className
             )}>
                 {children}
