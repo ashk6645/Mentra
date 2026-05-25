1535db434c9b2d7c22084663e047119cb89a2af7
Ashutosh Pratap Singh <112776372+ashk6645@users.noreply.github.com>
2026-05-25 14:08:15 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -42,6 +42,30 @@ const nextConfig: NextConfig = {
       }
     ]
   },
+
+       /** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: 'frame-ancestors *',
+          },
+        ],
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
+
   
   // Image optimization
   images: {
