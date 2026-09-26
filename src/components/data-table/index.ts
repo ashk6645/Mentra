e08c4a13058ba373e2dee5bf42171910a1ba883f
Ashutosh <ashk6645@gmail.com>
2026-09-26 20:52:41 +0530

    Export the data table from one entry point

---
+++ src/components/data-table/index.ts
@@ -0,0 +1,3 @@
+export { DataTable, type DataTableProps } from './data-table'
+export { compareValues, sortRows, nextSort } from './sort'
+export type { ColumnDef, RowGroup, SortDirection, SortState, SortValue } from './types'
